779698af4ecefd83f29a752e02f81a2fea543f9d
Roman <gurman3211@gmail.com>
2021-05-17 22:57:39 +0300

    Update PropertySettings.java

--- src/com/gurman/PropertySettings.java
+++ src/com/gurman/PropertySettings.java
@@ -13,10 +13,8 @@ class PropertySettings {
 
     PropertySettings(String prop_path) {
         path = prop_path;
-        //Создаем объект свойст
         properties = new Properties();
         File file = new File(path);
-        //Загружаем свойства из файла
         try {
             properties.load(new FileInputStream(file));
         } catch (IOException e) {
@@ -25,29 +23,23 @@ class PropertySettings {
     }
 
     String getSettings(String key) {
-        //Получаем в переменную значение конкретного свойства
         return properties.getProperty(key);
     }
 
     Boolean getSettingsBool(String key) {
-        //Получаем в переменную значение конкретного свойства
         return properties.getProperty(key).equals("1");
     }
 
     Float getSettingsFloat(String key) {
-        //Получаем в переменную значение конкретного свойства
         return Float.valueOf(properties.getProperty(key));
     }
 
     Integer getSettingsInt(String key) {
-        //Получаем в переменную значение конкретного свойства
         return Integer.parseInt(properties.getProperty(key));
     }
 
     void setProperties(String key, String value) {
-        //Устанавливаем значение свойста
         properties.setProperty(key, value);
-        //Сохраняем свойства в файл.
         try {
             properties.store(new FileOutputStream(new File(path)), null);
         } catch (IOException e) {
